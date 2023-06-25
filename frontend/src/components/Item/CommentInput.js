7fbe66fd8764d866f90b8ed2677507dffa512c75
Christian Wamalwa <bobitlmr188221@spu.ac.ke>
2023-06-25 15:36:34 +0000

    Fix bug in code

--- frontend/src/components/Item/CommentInput.js
+++ frontend/src/components/Item/CommentInput.js
@@ -8,9 +8,9 @@ const mapDispatchToProps = (dispatch) => ({
 });
 
 const CommentInput = ({ slug, onSubmit, currentUser }) => {
+  const [body, setBody] = useState();
 
-  const [body, setBody] = useState("");
-  const createComment = (ev) => {
+  const createComment = async (ev) => {
     ev.preventDefault();
     agent.Comments.create(slug, {
       body: body,
@@ -19,32 +19,30 @@ const CommentInput = ({ slug, onSubmit, currentUser }) => {
     });
     setBody("");
   };
+
+  return (
+    <form className="card comment-form m-2" onSubmit={createComment}>
+      <div className="card-block">
+        <textarea
+          className="form-control"
+          placeholder="Write a comment..."
+          value={body}
+          onChange={setBody}
+          rows="3"
+        ></textarea>
+      </div>
+      <div className="card-footer">
+        <img
+          src={currentUser.image}
+          className="user-pic mr-2"
+          alt={currentUser.username}
+        />
+        <button className="btn btn-sm btn-primary" type="submit">
+          Post Comment
+        </button>
+      </div>
+    </form>
+  );
 }
-
-return (
-  <form className="card comment-form m-2" onSubmit={createComment}>
-    <div className="card-block">
-      <textarea
-        className="form-control"
-        placeholder="Write a comment..."
-        value={body}
-        onChange={setBody}
-        rows="3"
-      ></textarea>
-    </div>
-    <div className="card-footer">
-      <img
-        src={currentUser.image}
-        className="user-pic mr-2"
-        alt={currentUser.username}
-      />
-      <button className="btn btn-sm btn-primary" type="submit">
-        Post Comment
-      </button>
-    </div>
-  </form>
-);
-
-
 
 export default connect(() => ({}), mapDispatchToProps)(CommentInput);
